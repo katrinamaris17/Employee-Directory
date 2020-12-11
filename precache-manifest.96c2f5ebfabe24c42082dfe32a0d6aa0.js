f698c810e9d4eaef07e62237fb4037139d4e1ee2
Katrina Arando <katrinamaris17@gmail.com>
2020-12-11 07:06:25 -0800

    Updates

---
+++ precache-manifest.96c2f5ebfabe24c42082dfe32a0d6aa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae5a08d082c7269f4542dd823b18daf4",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "14c7594c1807102eb081",
+    "url": "/Employee-Directory/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "81344f12cc43e5597837",
+    "url": "/Employee-Directory/static/css/main.16f2466d.chunk.css"
+  },
+  {
+    "revision": "14c7594c1807102eb081",
+    "url": "/Employee-Directory/static/js/2.0a6ff45d.chunk.js"
+  },
+  {
+    "revision": "81344f12cc43e5597837",
+    "url": "/Employee-Directory/static/js/main.3ae9481b.chunk.js"
+  },
+  {
+    "revision": "1794aedb7da2e07d4918",
+    "url": "/Employee-Directory/static/js/runtime~main.51f3d348.js"
+  }
+]);
