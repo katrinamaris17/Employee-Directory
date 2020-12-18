bdf05aa2d292408c22b0b6d5940e5c5f7c9ef8d4
Katrina Arando <katrinamaris17@gmail.com>
2020-12-17 21:58:20 -0800

    added styling, cleaned-up code, completed README

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Main from "./components/Main";
-import Wrapper from "./components/Wrapper";
+import Container from "./components/Container";
 import Header from "./components/Header";
 
 import "./App.css";
@@ -8,10 +8,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Wrapper>
+      <Container>
         <Header />
         <Main />
-      </Wrapper>
+      </Container>
     </div>
   );
 }
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export default {
-  // Gets all users
+  // Generates up to 200 users when page is loaded
   getUsers: function() {
     return axios.get("https://randomuser.me/api/?results=200&nat=us");
   }
 };
+
